ace9d48e41f423be6d9455150482310539147994
lwbcaudio <152240639+lwbcaudio@users.noreply.github.com>
2023-11-28 12:44:49 -0700

    Update app.js
    base64 added and outbound webhook

--- app.js
+++ app.js
@@ -6,6 +6,32 @@ app.use(express.json()); // for parsing application/json
 app.post('/webhook', (req, res) => {
   // This is where you'll handle the incoming webhook data
   console.log(req.body);
+
+  // Encode the JSON string to base64
+  const jsonString = JSON.stringify(req.body);
+  const base64String = Buffer.from(jsonString).toString('base64');
+
+  // Create the new JSON object
+  const newJson = {
+    cid: req.body.cid,
+    qrdata: base64String
+  };
+
+  // Send the new JSON object back as a webhook
+  // You'll need to replace 'your-webhook-url' with the actual URL of your webhook
+  fetch('your-webhook-url', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newJson)
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
   res.status(200).end(); // Responding is important
 });
 
