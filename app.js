8f38bde679b9a60769b63b49a2666d8ccfa9e360
lwbcaudio <152240639+lwbcaudio@users.noreply.github.com>
2023-11-29 23:36:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.post('/qr-gen', async (req, res) => {
     const qrpromohook = req.body.promohook;
     const qrname = req.body.id;
     const qrfile = qrname + '.png';
-    const hexString = '// qrdata 
+    const hexString = 'bob alksfj;alksdjf;alksfdj;alkdjf'; //'https://web-hook-qr.onrender.com/qr?qrdata=' + req.body.qrdata;
 
     // Generate QR code
     await qrcode.toFile(qrfile, hexString);
